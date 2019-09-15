7e758319f4a331f4546360f1e005588091ba9931
Paulo Gonçalo <paulo.goncalo@gmail.com>
2019-09-15 13:10:38 -0300

    first commit

---
+++ options.js
@@ -0,0 +1,31 @@
+﻿
+// Saves options to chrome.storage
+function save_options() {
+  var maxCharQuantity = document.getElementById('character-quantity').value;
+  chrome.storage.sync.set({
+    'maxCharQuantityStored': maxCharQuantity
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Opção salva.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 2000);
+  });
+}
+
+
+// Restores input state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  chrome.storage.sync.get({
+    'maxCharQuantityStored': 300
+  }, function(items) {
+    document.getElementById('character-quantity').value = items.maxCharQuantityStored;
+
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save-button').addEventListener('click',
+    save_options);
---
+++ textao-content.js
@@ -0,0 +1,72 @@
+﻿
+;(function( doc ) {
+
+	//get the maximum character quantity
+	var maxCharQuantity;
+	chrome.storage.sync.get({
+		'maxCharQuantityStored' : 300
+	}, function(items) {
+		maxCharQuantity = items.maxCharQuantityStored;
+		});
+
+	//set interval to run the posts verification
+	window.setInterval(function() {
+	   	   
+		   
+	//posts verification	   
+	$("._3ccb:not(._5pbu) > .fbUserContent._5pcr:not(.tb-verified)").each(function(index){
+	
+		var charSum = 0;
+   
+		$(this).addClass("tb-verified");
+   
+		$("._5pbx.userContent",this).each(function(index){		
+			charSum = charSum + $(this).text().length;
+	   }); 
+	   
+	   if (charSum >= maxCharQuantity) {
+		
+			$(this).hide();
+			
+			// add div with warning message
+	   		$(this).parent().append("<div class=\"textao-warning\">Textão bloqueado!<a href=\"#\">Visualizar</a></div>");
+			$(this).addClass("tb-blocked");
+			
+			$(this).parent().find(".textao-warning a").click(function() {
+				$(this).parent().hide();
+				$(this).parent().parent().find(".tb-blocked").show();	
+			});
+	   }
+   }); 
+   
+	
+	//comments verification
+	$(".UFICommentBody:not(.tb-comments-verified)").each(function(index){ //UFIRow
+		
+		var charSum = 0;
+
+		$(this).addClass("tb-comments-verified");
+		
+		$("span",this).each(function(index){		
+			charSum = charSum + $(this).text().length;
+		}); 
+		
+		if (charSum >= maxCharQuantity) {
+			//add div with warning message
+			console.log($(this).parent().parent().parent().parent().parent().parent().parent().parent());
+			$(this).parent().parent().parent().parent().parent().parent().parent().parent().hide();
+			$(this).parent().parent().parent().parent().parent().parent().parent().parent().parent().append("<div class=\"textao-warning\">Textão bloqueado!<a href=\"#\">Visualizar</a></div>");
+			$(this).parent().parent().parent().parent().parent().parent().parent().parent().addClass("tb-blocked");
+			
+			$(this).parent().parent().parent().parent().parent().parent().parent().parent().parent().find(".textao-warning a").click(function() {
+				$(this).parent().hide();
+				$(this).parent().parent().find(".tb-blocked").show();	
+			});
+		} 
+		
+	}); 
+	   
+	   
+   }, 1000);
+})( document );
+
